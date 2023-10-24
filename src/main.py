8e643f8cac91b7e0f3b68d09536582ceb6e6ac80
Vlad Morozov <v@morosoff.ru>
2023-10-24 11:14:42 +0300

    Tested with black, isort, mypy

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 from src.widget import format_payment_info, get_date_from_list
 
 if __name__ == "__main__":
-    print(format_payment_info('Maestro 1596837868705199'))
-    print(format_payment_info('Счет 64686473678894779589'))
-    print(format_payment_info('Visa Gold 5999414228426353'))
-    print(format_payment_info('5999414228426353'))
-    print(format_payment_info('64686473678894779589'))
+    print(format_payment_info("Maestro 1596837868705199"))
+    print(format_payment_info("Счет 64686473678894779589"))
+    print(format_payment_info("Visa Gold 5999414228426353"))
+    print(format_payment_info("5999414228426353"))
+    print(format_payment_info("64686473678894779589"))
 
-    print(get_date_from_list('2018-07-11T02:26:18.671407'))
+    print(get_date_from_list("2018-07-11T02:26:18.671407"))
--- src/widget.py
+++ src/widget.py
@@ -6,13 +6,13 @@ def format_payment_info(info: str) -> str:
     и возвращает эту строку с замаскированным номером карты/счета
     в формате XXXX XX** **** XXXX для карты и
     в формате **XXXX для счета."""
-    list_of_info = info.split(' ')
+    list_of_info = info.split(" ")
     payment_number = list_of_info.pop()
-    payment_type = ' '.join(list_of_info)
-    if payment_type == '' and len(payment_number) == 16:
-        payment_type = 'AnyCard'
-    elif payment_type == '' and len(payment_number) == 20:
-        payment_type = 'Счет'
+    payment_type = " ".join(list_of_info)
+    if payment_type == "" and len(payment_number) == 16:
+        payment_type = "AnyCard"
+    elif payment_type == "" and len(payment_number) == 20:
+        payment_type = "Счет"
     if payment_number.isdigit() and len(payment_number) == 16:
         list_of_segments = re.findall(".{%s}" % 4, payment_number)
         list_of_segments[1] = list_of_segments[1][:2] + "**"
@@ -27,8 +27,8 @@ def format_payment_info(info: str) -> str:
 def get_date_from_list(string: str) -> str:
     """принимает на вход строку, вида '2018-07-11T02:26:18.671407'
     и возвращает строку с датой в виде '11.07.2018'"""
-    date_old_string = string.split('T')[0]
-    date = date_old_string.split('-')[2]
-    month = date_old_string.split('-')[1]
-    year = date_old_string.split('-')[0]
-    return f'{date}.{month}.{year}'
+    date_old_string = string.split("T")[0]
+    date = date_old_string.split("-")[2]
+    month = date_old_string.split("-")[1]
+    year = date_old_string.split("-")[0]
+    return f"{date}.{month}.{year}"
