bb70fd4ef9a034438b5442ec4c9b384095b59c97
Vlad Morozov <v@morosoff.ru>
2023-10-28 19:39:06 +0300

    add HW2 solution in main.py and processing.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
 from src.hw_add_tasks import get_format_string, get_nums_multiple
+from src.processing import get_formatted_list, get_formatted_list_by_date
 from src.widget import format_payment_info, get_date_from_list
 
+input_dictionary = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+
+
 if __name__ == "__main__":
+    # ДОМАШНЕЕ ЗАДАНИЕ №1
     print(format_payment_info("Maestro 1596837868705199"))
     print(format_payment_info("Счет 64686473678894779589"))
     print(format_payment_info("Visa Gold 5999414228426353"))
@@ -18,3 +26,8 @@ if __name__ == "__main__":
     print(get_nums_multiple([-5, -7, -9, -13]))
     print(get_nums_multiple([1, 2]))
     print(get_nums_multiple([4]))
+
+    # Задание 1. Проект (ДОМАШНЕЕ ЗАДАНИЕ №2)
+    print(get_formatted_list(input_dictionary, 'EXECUTED'))
+    print(get_formatted_list_by_date(input_dictionary))
+    print(get_formatted_list_by_date(input_dictionary, False))
---
+++ src/processing.py
@@ -0,0 +1,16 @@
+def get_formatted_list(any_list: list[dict, dict], status: str = None) -> list[dict]:
+    """выбирает из списка словарей, словари с определенным значением (задается вторым аргументом)"""
+    new_list = []
+    for one_dict in any_list:
+        if one_dict['state'] == status:
+            new_list.append(one_dict)
+    return new_list
+
+
+def get_formatted_list_by_date(any_list: list[dict, dict], sort_up: bool = True) -> list[dict]:
+    """упорядочивает словари по дате (со вторым параметром сортировка от меньшей к большей и наоборот)"""
+    if sort_up:
+        sorted_list = sorted(any_list, key=lambda x: x["date"])
+    else:
+        sorted_list = sorted(any_list, key=lambda x: x["date"], reverse=True)
+    return sorted_list
