2d5cf8e28186ed597a1bbc032db4369d905718c4
Vlad Morozov <v@morosoff.ru>
2023-10-28 19:43:11 +0300

    checked with black, isort, mypy

--- src/processing.py
+++ src/processing.py
@@ -1,13 +1,13 @@
-def get_formatted_list(any_list: list[dict, dict], status: str = None) -> list[dict]:
+def get_formatted_list(any_list: list[dict], status: str = "") -> list[dict]:
     """выбирает из списка словарей, словари с определенным значением (задается вторым аргументом)"""
     new_list = []
     for one_dict in any_list:
-        if one_dict['state'] == status:
+        if one_dict["state"] == status:
             new_list.append(one_dict)
     return new_list
 
 
-def get_formatted_list_by_date(any_list: list[dict, dict], sort_up: bool = True) -> list[dict]:
+def get_formatted_list_by_date(any_list: list[dict], sort_up: bool = True) -> list[dict]:
     """упорядочивает словари по дате (со вторым параметром сортировка от меньшей к большей и наоборот)"""
     if sort_up:
         sorted_list = sorted(any_list, key=lambda x: x["date"])
