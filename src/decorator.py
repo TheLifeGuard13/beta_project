5eb1df76032f4a7d8dd85481f9520d6eb52e4b51
Vlad Morozov <v@morosoff.ru>
2023-11-12 11:16:58 +0300

    corrected decorator.py and test_decorator.py

--- src/decorator.py
+++ src/decorator.py
@@ -7,29 +7,19 @@ def log(*, filename: str = "") -> typing.Any:
     def wrapper(any_func: typing.Callable) -> typing.Callable:
         @wraps(any_func)
         def inner(*args: typing.Any, **kwargs: typing.Any) -> typing.Any:
+            date_now = datetime.datetime.now().strftime("%y-%m-%d %H:%M:%S")
             try:
                 result = any_func(*args, **kwargs)
-                if filename == "":
-                    print(f"""{datetime.datetime.now().strftime("%y-%m-%d %H:%M:%S")} {any_func.__name__} ok""")
-                    return f"Итого = {result}"
-                else:
-                    with open(filename, "a", encoding="utf-8") as file:
-                        file.write(f'{datetime.datetime.now().strftime("%y-%m-%d %H:%M:%S")} {any_func.__name__} ok\n')
-                    return f"Итого = {result}"
+                log_message = f"""{date_now} {any_func.__name__} ok"""
             except Exception as error:
-                if filename == "":
-                    print("Ошибка, см. консоль")
-                    print(
-                        f'{datetime.datetime.now().strftime("%y-%m-%d %H:%M:%S")} {any_func.__name__} \
-error: <{error}>. Inputs: {args}, {kwargs}'
-                    )
-                else:
-                    print("Ошибка, см. лог файл")
-                    with open(filename, "a", encoding="utf-8") as file:
-                        file.write(
-                            f'{datetime.datetime.now().strftime("%y-%m-%d %H:%M:%S")} {any_func.__name__} \
-                             error: <{error}>. Inputs: {args}, {kwargs}\n'
-                        )
+                log_message = f"""{date_now} {any_func.__name__} error: <{error}>. Inputs: {args}, {kwargs}"""
+                result = None
+            if filename:
+                with open(filename, "a", encoding="utf-8") as file:
+                    file.write(log_message)
+            else:
+                print(log_message)
+            return result
 
         return inner
 
@@ -42,4 +32,4 @@ def my_function(x: typing.Any, y: typing.Any) -> typing.Any:
     return x + y
 
 
-print(my_function(1, 1))
+print(my_function("1", 1))
