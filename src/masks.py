8ec5207845c4119d84b605dd7908208b28e2edb5
Vlad Morozov <v@morosoff.ru>
2023-11-17 22:42:40 +0300

    Added logging to utils.py and masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,4 +1,19 @@
 import re
+import logging
+import os
+from pathlib import Path
+
+DATA_PATH_LOG = Path(__file__).parent.parent.joinpath("data", "masks_log.txt")
+logger = logging.getLogger('__masks__')
+
+if os.path.exists(DATA_PATH_LOG):
+    os.remove(DATA_PATH_LOG)
+
+file_handler = logging.FileHandler(DATA_PATH_LOG)
+file_formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.INFO)
 
 
 def format_card_number(number: str) -> str:
@@ -8,8 +23,10 @@ def format_card_number(number: str) -> str:
         list_of_segments = re.findall(".{%s}" % 4, number)
         list_of_segments[1] = list_of_segments[1][:2] + "**"
         list_of_segments[2] = "****"
+        logger.info('Card number OK')
         return " ".join(list_of_segments)
     else:
+        logger.info('Wrong card number input')
         return "Это не номер карты!"
 
 
@@ -17,6 +34,14 @@ def format_acc_number(number: str) -> str:
     """Функция принимает на вход номер счёта и
     возвращает его маску в формате **XXXX."""
     if number.isdigit() and len(number) == 20:
+        logger.info('Acc number OK')
         return "**" + number[16:]
     else:
+        logger.info('Wrong acc number input')
         return "Это не номер счета!"
+
+
+# print(format_card_number("1234567890123456"))
+# print(format_card_number("111"))
+# print(format_acc_number("12345678901234567890"))
+# print(format_acc_number("222"))
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,23 @@
 import json
+import logging
 import random
 import typing
+import os
+from pathlib import Path
+
+from config import DATA_PATH_TEST, DATA_PATH_TEST2
+
+DATA_PATH_LOG = Path(__file__).parent.parent.joinpath("data", "utils_log.txt")
+logger = logging.getLogger('__utils__')
+
+if os.path.exists(DATA_PATH_LOG):
+    os.remove(DATA_PATH_LOG)
+
+file_handler = logging.FileHandler(DATA_PATH_LOG)
+file_formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.INFO)
 
 
 def load_file(filename: str) -> typing.Any:
@@ -8,15 +25,19 @@ def load_file(filename: str) -> typing.Any:
     try:
         with open(filename, encoding="utf-8") as file:
             file_data = json.load(file)
+            logger.info('JSON OK')
     except json.JSONDecodeError:
+        logger.info('JSONDecodeError')
         file_data = []
     except FileNotFoundError:
+        logger.info('FileNotFoundError')
         file_data = []
     return file_data
 
 
 def pick_one_operation(operations: list[dict]) -> dict:
     """выбирает случайную транзакцию из списка операций"""
+    logger.info('pick one OK')
     return random.choice(operations)
 
 
@@ -24,8 +45,22 @@ def check_currency(operation: dict) -> float:
     """вычисляет сумму транзакции в рублях или выдает ошибку, если валюта - не рубли"""
     if operation:
         if operation["operationAmount"]["currency"]["code"] == "RUB":
+            logger.info('Currency OK')
             return float(operation["operationAmount"]["amount"])
         else:
+            logger.info('Wrong currency')
             raise ValueError("Транзакция выполнена не в рублях. Укажите транзакцию в рублях")
     else:
+        logger.info('Operation not found')
         raise Exception("Транзакция не найдена")
+
+
+# print(load_file(DATA_PATH_TEST))    # [dict, dict ...]
+# print(load_file(DATA_PATH_TEST2))  # []
+# print(load_file("./src/test_json.json"))  # []
+
+# print(pick_one_operation(load_file(DATA_PATH_TEST)))  #
+
+# print(check_currency({'operationAmount': {'currency': {'code': 0}}}))  #
+# print(check_currency({'operationAmount': {'currency': {'code': 'RUB'}}}))  #
+# print(check_currency({}))  #
