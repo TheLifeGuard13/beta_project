17c75983e50e24f387778c30340347109b02d2d3
Vlad Morozov <morozov90vlad@yandex.ru>
2023-11-25 10:38:48 +0300

    Completed HW9 (processing.py) Completed HW9 tests and checked with isort, black, flake8, mypy Small fixes in previous HW (moving testing files to new directory

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ ROOT_PATH = Path(__file__).parent
 DATA_PATH = ROOT_PATH.joinpath("data", "operations.json")
 
 ROOT_PATH_TEST = Path(__file__).parent
-DATA_PATH_TEST = ROOT_PATH_TEST.joinpath("tests", "test_json.json")
+DATA_PATH_TEST = ROOT_PATH_TEST.joinpath("tests", "tests_data", "test_json.json")
 
 ROOT_PATH_TEST2 = Path(__file__).parent
-DATA_PATH_TEST2 = ROOT_PATH_TEST2.joinpath("tests", "test2_json.json")
+DATA_PATH_TEST2 = ROOT_PATH_TEST2.joinpath("tests", "tests_data", "test2_json.json")
 
 ROOT_PATH_CSV = Path(__file__).parent
 DATA_PATH_CSV = ROOT_PATH_CSV.joinpath("data", "transactions.csv")
@@ -16,4 +16,4 @@ ROOT_PATH_XLS = Path(__file__).parent
 DATA_PATH_XLS = ROOT_PATH_XLS.joinpath("data", "transactions_excel.xlsx")
 
 ROOT_PATH_TEST3 = Path(__file__).parent
-DATA_PATH_TEST3 = ROOT_PATH_TEST3.joinpath("tests", "test_json3.json")
+DATA_PATH_TEST3 = ROOT_PATH_TEST3.joinpath("tests", "tests_data", "test_json3.json")
--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,6 @@
+import re
+
+
 def get_formatted_list(any_list: list[dict], status: str = "") -> list[dict]:
     """выбирает из списка словарей, словари с определенным значением (задается вторым аргументом)"""
     new_list = []
@@ -14,3 +17,20 @@ def get_formatted_list_by_date(any_list: list[dict], sort_up: bool = True) -> li
     else:
         sorted_list = sorted(any_list, key=lambda x: x["date"], reverse=True)
     return sorted_list
+
+
+def find_operation_by_description(any_list: list[dict], description: str) -> list[dict]:
+    """принимает список словарей с данными о банковских операциях и строку поиска и
+    возвращает список словарей, у которых в описании есть данная строка"""
+    return [x for x in any_list if x if re.search(description, x["description"], flags=re.I)]
+
+
+def find_categories(any_list: list[dict], category: dict) -> dict:
+    """принимать список словарей с данными о банковских операциях и словарь категорий операций
+    и возвращать словарь, в котором ключи — это названия категорий, а значения — это количество
+    операций в каждой категории."""
+    cat_list = [value for key, value in category.items()]
+    dict_ = {}
+    for cat in cat_list:
+        dict_[cat] = len([x for x in any_list if x if re.search(cat, x["description"], flags=re.I)])
+    return dict_
